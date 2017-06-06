56af6aa0903fdf671ad7360016e4d1178756f1e2
DanglaGT <n.n3ls0n@gmail.com>
2017-06-05 21:30:50 -0500

    Create print_value_from_key.py

---
+++ dictionaries/print_value_from_key.py
@@ -0,0 +1,6 @@
+
+# this code will print the value assigned to a specific key in a dictionary
+user_dict = {'fnam': 'nick', 'lname': 'nelson', 'age': 28,
+    'username': 'slugfx'}
+
+print(user_dict['username'])
