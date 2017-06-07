7083ea64cc40cd497391fee0540263657137810e
DanglaGT <n.n3ls0n@gmail.com>
2017-06-06 21:30:10 -0500

    Create left_join.py

---
+++ pandas/left_join.py
@@ -0,0 +1,20 @@
+# this code demonstrates how to left join one
+# pandas dataframe to another one
+
+import pandas as pd
+import numpy as np
+
+df1 = pd.DataFrame({'key': ['A', 'B', 'C', 'D'],
+                    'value': np.random.randn(4)})
+
+df2 = pd.DataFrame({'key': ['B', 'D', 'D', 'E'],
+                    'value': np.random.randn(4)})
+
+print(df1)
+print("-" * 50)
+print(df2)
+print("-" * 50)
+
+left_join = df1.merge(df2, on=['key'], how='left')
+print("Left Join:")
+print(left_join)
