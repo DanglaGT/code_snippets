1c851b6b4e2149de7598e34671e4a0cf85d979b2
DanglaGT <n.n3ls0n@gmail.com>
2017-06-06 21:59:29 -0500

    Create statistic_summary.py

---
+++ pandas/statistic_summary.py
@@ -0,0 +1,16 @@
+# the describe() function shows a quick statistical
+# summary of your dataframe (numeric values only)
+
+import pandas as pd
+
+# using a dictionary to create a pandas dataframe
+# the keys are the column names and the values are the data
+df = pd.DataFrame({
+    'gender': ['Male', 'Male', 'Female', 'Male', 'Female'],
+    'age': [28, 21, 19, 18, 30],
+    'bday': [pd.Timestamp('1989-05-05'), pd.Timestamp('2013-01-15'),
+             pd.Timestamp('2019-01-15'), pd.Timestamp('2010-01-15'),
+             pd.Timestamp('2013-09-08')]
+    })
+
+print(df.describe())
